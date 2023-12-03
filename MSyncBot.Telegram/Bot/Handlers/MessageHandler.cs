718d929a68b7b9bb11cf037ecdf7700016e07463
Neelov Eugene <neelov.ev@mail.ru>
2023-12-03 16:37:04 +0300

    Added message handler

---
+++ MSyncBot.Telegram/Bot/Handlers/MessageHandler.cs
@@ -0,0 +1,36 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace MSyncBot.Telegram.Bot.Handlers;
+
+public class MessageHandler
+{
+    public async Task MessageHandlerAsync(ITelegramBotClient botClient, Update update)
+    {
+        try
+        {
+            var message = update.Message;
+
+            Bot.Logger.LogInformation("receive update");
+            
+            switch (message)
+            {
+                case { Type: MessageType.Text }:
+                    var text = message.Text;
+                    if (IsBotCommand(text))
+                        await new CommandsHandler().CommandsHandlerAsync(botClient, update);
+                    return;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        catch (Exception ex)
+        {
+            Bot.Logger?.LogError(ex.ToString());
+        }
+    }
+    
+    private bool IsBotCommand(string text) => !string.IsNullOrEmpty(text) && text.StartsWith("/");
+}
