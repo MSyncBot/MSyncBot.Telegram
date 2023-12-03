1922a31ed6a2dacc5c17fce12bed61221bd096fe
Neelov Eugene <neelov.ev@mail.ru>
2023-12-03 16:36:39 +0300

    Added error handler

---
+++ MSyncBot.Telegram/Bot/Handlers/General/ErrorHandler.cs
@@ -0,0 +1,22 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+
+namespace MSyncBot.Telegram.Bot.Handlers.General;
+
+public class ErrorHandler
+{
+    public Task GetApiError(ITelegramBotClient botClient, Exception exception,
+        CancellationToken cancellationToken)
+    {
+        var errorMessage = exception switch
+        {
+            ApiRequestException apiRequestException
+                => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+            _ => exception.ToString()
+        };
+
+        Bot.Logger?.LogError(errorMessage);
+
+        return Task.CompletedTask;
+    }
+}
