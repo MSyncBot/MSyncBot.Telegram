1e2b7286d5a0ee039c515a6a8ed2f8bdab1de1aa
Neelov Eugene <neelov.ev@mail.ru>
2023-12-03 16:36:30 +0300

    Added commands handler

---
+++ MSyncBot.Telegram/Bot/Handlers/CommandsHandler.cs
@@ -0,0 +1,33 @@
+﻿using MSyncBot.Telegram.Bot.Commands;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace MSyncBot.Telegram.Bot.Handlers;
+
+public class CommandsHandler
+{
+    public async Task CommandsHandlerAsync(ITelegramBotClient botClient, Update update)
+    {
+        try
+        {
+            var message = update.Message;
+            var command = message.Text;
+            var user = message.From;
+            
+            Bot.Logger.LogInformation(
+                $"{user.FirstName} {user.LastName} | {user.Username} ({user.Id}) использовал команду {command}");
+
+            switch (command)
+            {
+                case "/start":
+                    await new StartCommand().SendWelcomeMessageAsync(botClient, update);
+                    return;
+            }
+        }
+        catch (Exception ex)
+        {
+            Bot.Logger.LogError(ex.ToString());
+        }
+    }
+}
