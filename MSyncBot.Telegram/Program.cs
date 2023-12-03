3fd964663948f3c41cb752d437a3014356f1317c
Neelov Eugene <neelov.ev@mail.ru>
2023-12-03 16:38:31 +0300

    Added main class

--- MSyncBot.Telegram/Program.cs
+++ MSyncBot.Telegram/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using MLoggerService;
 
-Console.WriteLine("Hello, World!");
+namespace MSyncBot.Telegram;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var logger = new MLogger();
+        
+        logger.LogProcess("Initializing database...");
+        var database = new MDatabase.
+            MDatabase(
+                "####",
+                "####",
+                "####",
+                "####");
+
+        //await database.PingAsync();
+        logger.LogSuccess("Database has been initalized");
+
+        var bot = new Bot.Bot("####", logger, database);
+        await bot.StartAsync();
+        await Task.Delay(-1);
+    }
+}
