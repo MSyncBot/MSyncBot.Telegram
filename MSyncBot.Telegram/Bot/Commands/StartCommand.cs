e058f0657eeba7ade9e02fa6cd915e73bef0e3ef
Neelov Eugene <neelov.ev@mail.ru>
2023-12-03 16:37:39 +0300

    Added base start command

---
+++ MSyncBot.Telegram/Bot/Commands/StartCommand.cs
@@ -0,0 +1,22 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace MSyncBot.Telegram.Bot.Commands;
+
+public class StartCommand
+{
+    public async Task SendWelcomeMessageAsync(ITelegramBotClient botClient, Update update)
+    {
+        try
+        {
+            var message = update.Message;
+            var chat = message.Chat;
+            await botClient.SendTextMessageAsync(chat.Id,
+                "Hello!");
+        }
+        catch (Exception ex)
+        {
+            Bot.Logger.LogError(ex.ToString());
+        }
+    }
+}
