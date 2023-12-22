74dd007397b77f4f26d9c84f31b79abdcc8c8613
Neelov Eugene <neelov.ev@mail.ru>
2023-12-22 09:45:38 +0300

    Rework MediaFileHandler.cs for all types media and albums. Optimization code.

--- MSyncBot.Telegram/Bot/Handlers/MediaFileHandler.cs
+++ MSyncBot.Telegram/Bot/Handlers/MediaFileHandler.cs
@@ -17,36 +17,25 @@ namespace MSyncBot.Telegram.Bot.Handlers
         {
             try
             {
-                switch (message.Type)
+                if (!string.IsNullOrEmpty(message.MediaGroupId))
                 {
-                    case MessageType.Photo:
-                        await ProcessPhotoMessageAsync(botClient, message);
-                        return;
-
-                    case MessageType.Video:
-                        await ProcessVideoMessageAsync(botClient, message);
-                        return;
-                    
-                    case MessageType.Animation:
-                        await ProcessAnimationMessageAsync(botClient, message);
-                        return;
-                    
-                    case MessageType.Document:
-                        await ProcessDocumentMessageAsync(botClient, message);
-                        return;
-                    
-                    case MessageType.Audio:
-                        await ProcessAudioMessageAsync(botClient, message);
-                        return;
-                    
-                    case MessageType.Voice:
-                        await ProcessVoiceMessageAsync(botClient, message);
-                        return;
-                    
-                    case MessageType.VideoNote:
-                        await ProcessVideoNoteMessageAsync(botClient, message);
-                        return;
+                    await AlbumHandlerAsync(botClient, message);
+                    return;
                 }
+
+                var user = message.From;
+                
+                var fileInfo = GetFileInfo(message);
+                var file = await DownloadFileToRamAsync(botClient, fileInfo.id);
+                var fileMessage = new Types.Message("MSyncBot.Telegram",
+                    1,
+                    SenderType.Telegram,
+                    fileInfo.messageType,
+                    new User(user.FirstName, user.LastName, user.Username, (ulong?)user.Id));
+                fileMessage.MediaFiles.Add(file);
+
+                var jsonFileMessage = JsonSerializer.Serialize(fileMessage);
+                Bot.Server.SendTextAsync(jsonFileMessage);
             }
             catch (Exception ex)
             {
@@ -54,46 +43,78 @@ namespace MSyncBot.Telegram.Bot.Handlers
             }
         }
 
-        private static async Task ProcessPhotoMessageAsync(ITelegramBotClient botClient, Message message)
+        private static (string id, Types.Enums.MessageType messageType) GetFileInfo(Message message)
         {
-            var photoId = message.Photo?.Last()?.FileId;
-            if (string.IsNullOrEmpty(photoId)) return;
-
-            var photoInfo = await botClient.GetFileAsync(photoId);
-            var photoPath = photoInfo.FilePath;
-
-            using var photoStream = new MemoryStream();
-            await botClient.DownloadFileAsync(filePath: photoPath, destination: photoStream);
-            photoStream.Seek(0, SeekOrigin.Begin);
-            var photoBytes = photoStream.ToArray();
-            var photoFile = new MediaFile(Guid.NewGuid().ToString(),
-                ".png",
-                photoBytes,
-                FileType.Photo);
-            
-            var photoMessage = new Types.Message("MSyncBot.Telegram",
-                1,
-                SenderType.Telegram,
-                Types.Enums.MessageType.Photo,
-                new Types.User(message.From.FirstName));
-            photoMessage.MediaFiles.Add(photoFile);
-            
-            string? jsonPhotoMessage;
-            if (string.IsNullOrEmpty(message.MediaGroupId))
+            var fileId = string.Empty;
+            var messageType = Types.Enums.MessageType.Unknown;
+            switch (message.Type)
             {
-                jsonPhotoMessage = JsonSerializer.Serialize(photoMessage);
-                Bot.Server.SendTextAsync(jsonPhotoMessage);
-                return;
+                case MessageType.Photo:
+                    fileId = message.Photo.Last().FileId;
+                    messageType = Types.Enums.MessageType.Photo;
+                    break;
+                case MessageType.Audio:
+                    fileId = message.Audio.FileId;
+                    messageType = Types.Enums.MessageType.Audio;
+                    break;
+                case MessageType.Video:
+                    fileId = message.Video.FileId;
+                    messageType = Types.Enums.MessageType.Voice;
+                    break;
+                case MessageType.Voice:
+                    fileId = message.Voice.FileId;
+                    messageType = Types.Enums.MessageType.Voice;
+                    break;
+                case MessageType.Document:
+                    fileId = message.Document.FileId;
+                    messageType = Types.Enums.MessageType.Document;
+                    break;
+                case MessageType.VideoNote:
+                    fileId = message.VideoNote.FileId;
+                    messageType = Types.Enums.MessageType.VideoNote;
+                    break;
+                case MessageType.Animation:
+                    fileId = message.Animation.FileId;
+                    messageType = Types.Enums.MessageType.Animation;
+                    break;
             }
 
+            return (fileId, messageType);
+        }
+        
+        private static async Task<MediaFile?> DownloadFileToRamAsync(ITelegramBotClient botClient, string fileId)
+        {
+            if (string.IsNullOrEmpty(fileId))
+                return null;
+
+            var fileInfo = await botClient.GetFileAsync(fileId);
+            var filePath = fileInfo.FilePath;
+
+            using var fileStream = new MemoryStream();
+            await botClient.DownloadFileAsync(filePath: filePath, destination: fileStream);
+            fileStream.Seek(0, SeekOrigin.Begin);
+
+            var fileBytes = fileStream.ToArray();
+            var file = new MediaFile(Guid.NewGuid().ToString(),
+                Path.GetExtension(filePath),
+                fileBytes);
+
+            return file;
+        }
+
+        private static async Task AlbumHandlerAsync(ITelegramBotClient botClient, Message message)
+        {
             if (!MediaFiles.TryGetValue(message.MediaGroupId, out var mediaGroupFiles))
             {
-                mediaGroupFiles = new List<MediaFile>();
+                mediaGroupFiles = [];
                 MediaFiles.Add(message.MediaGroupId, mediaGroupFiles);
             }
 
-            mediaGroupFiles.Add(photoFile);
+            var fileInfo = GetFileInfo(message);
+            var file = await DownloadFileToRamAsync(botClient, fileInfo.id);
             
+            mediaGroupFiles.Add(file);
+
             var albumSize = CountMediaFiles[message.MediaGroupId];
             if (!SendingMediaFiles.TryGetValue(message.MediaGroupId, out var sentFiles))
             {
@@ -108,242 +129,35 @@ namespace MSyncBot.Telegram.Bot.Handlers
 
             if (sentFiles == albumSize)
             {
-                photoMessage.MediaFiles.AddRange(mediaGroupFiles);
-                jsonPhotoMessage = JsonSerializer.Serialize(photoMessage);
-                Bot.Server.SendTextAsync(jsonPhotoMessage);
+                var user = message.From;
                 
+                var albumMessage = new Types.Message("MSyncBot.Telegram",
+                    1,
+                    SenderType.Telegram,
+                    Types.Enums.MessageType.Album,
+                    new User(user.FirstName, user.LastName, user.Username, (ulong?)user.Id));
+                albumMessage.MediaFiles.AddRange(mediaGroupFiles);
+                
+                var jsonAlbumMessage = JsonSerializer.Serialize(albumMessage);
+                Bot.Server.SendTextAsync(jsonAlbumMessage);
+
                 SendingMediaFiles.Remove(message.MediaGroupId);
                 MediaFiles.Remove(message.MediaGroupId);
             }
         }
 
-        private static async Task ProcessVideoMessageAsync(ITelegramBotClient botClient, Message message)
+        public static void CountingMediaFiles(string groupId)
         {
-            var videoId = message.Video?.FileId;
-            if (string.IsNullOrEmpty(videoId)) return;
+            if (string.IsNullOrEmpty(groupId))
+                return;
 
-            var videoInfo = await botClient.GetFileAsync(videoId);
-            var videoPath = videoInfo.FilePath;
-
-            using var videoStream = new MemoryStream();
-            await botClient.DownloadFileAsync(filePath: videoPath, destination: videoStream);
-            videoStream.Seek(0, SeekOrigin.Begin);
-
-            var videoBytes = videoStream.ToArray();
-            var videoFile = new MediaFile(Guid.NewGuid().ToString(),
-                ".mp4",
-                videoBytes,
-                FileType.Video);
-
-            string? jsonVideoMessage;
-            if (string.IsNullOrEmpty(message.MediaGroupId))
+            if (CountMediaFiles.TryGetValue(groupId, out var value))
             {
-                var videoMessage = new Types.Message("MSyncBot.Telegram",
-                    1,
-                    SenderType.Telegram,
-                    Types.Enums.MessageType.Video,
-                    new Types.User(message.From.FirstName));
-                videoMessage.MediaFiles.Add(videoFile);
-                jsonVideoMessage = JsonSerializer.Serialize(videoMessage);
-                Bot.Server.SendTextAsync(jsonVideoMessage);
+                CountMediaFiles[groupId] = ++value;
                 return;
             }
 
-            if (!MediaFiles.TryGetValue(message.MediaGroupId, out var mediaGroupFiles))
-            {
-                mediaGroupFiles = new List<MediaFile>();
-                MediaFiles.Add(message.MediaGroupId, mediaGroupFiles);
-            }
-
-            mediaGroupFiles.Add(videoFile);
-            jsonVideoMessage = JsonSerializer.Serialize(videoFile);
-
-            Bot.Server.SendTextAsync(jsonVideoMessage);
-        }
-
-        private static async Task ProcessAnimationMessageAsync(ITelegramBotClient botClient, Message message)
-        {
-            var animationId = message.Animation?.FileId;
-            if (string.IsNullOrEmpty(animationId)) return;
-
-            var animationInfo = await botClient.GetFileAsync(animationId);
-            var animationPath = animationInfo.FilePath;
-
-            using var animationStream = new MemoryStream();
-            await botClient.DownloadFileAsync(filePath: animationPath, destination: animationStream);
-            animationStream.Seek(0, SeekOrigin.Begin);
-
-            var animationBytes = animationStream.ToArray();
-            var animationFile = new MediaFile(Guid.NewGuid().ToString(),
-                ".gif",
-                animationBytes,
-                FileType.Animation);
-
-            string? jsonAnimationMessage;
-            if (string.IsNullOrEmpty(message.MediaGroupId))
-            {
-                var animationMessage = new Types.Message("MSyncBot.Telegram",
-                    1,
-                    SenderType.Telegram,
-                    Types.Enums.MessageType.Animation,
-                    new User(message.From.FirstName));
-                animationMessage.MediaFiles.Add(animationFile);
-                jsonAnimationMessage = JsonSerializer.Serialize(animationMessage);
-                Bot.Server.SendTextAsync(jsonAnimationMessage);
-                return;
-            }
-
-            if (!MediaFiles.TryGetValue(message.MediaGroupId, out var mediaGroupFiles))
-            {
-                mediaGroupFiles = new List<MediaFile>();
-                MediaFiles.Add(message.MediaGroupId, mediaGroupFiles);
-            }
-
-            mediaGroupFiles.Add(animationFile);
-            jsonAnimationMessage = JsonSerializer.Serialize(animationFile);
-
-            Bot.Server.SendTextAsync(jsonAnimationMessage);
-        }
-
-        private static async Task ProcessDocumentMessageAsync(ITelegramBotClient botClient, Message message)
-        {
-            var documentId = message.Document?.FileId;
-            if (string.IsNullOrEmpty(documentId)) return;
-
-            var documentInfo = await botClient.GetFileAsync(documentId);
-            var documentPath = documentInfo.FilePath;
-
-            using var documentStream = new MemoryStream();
-            await botClient.DownloadFileAsync(filePath: documentPath, destination: documentStream);
-            documentStream.Seek(0, SeekOrigin.Begin);
-
-            var documentBytes = documentStream.ToArray();
-            var documentFile = new MediaFile(Guid.NewGuid().ToString(),
-                Path.GetExtension(documentInfo.FilePath),
-                documentBytes,
-                FileType.Document);
-
-            string? jsonDocumentMessage;
-            if (string.IsNullOrEmpty(message.MediaGroupId))
-            {
-                var documentMessage = new Types.Message("MSyncBot.Telegram",
-                    1,
-                    SenderType.Telegram,
-                    Types.Enums.MessageType.Document,
-                    new Types.User(message.From.FirstName));
-                documentMessage.MediaFiles.Add(documentFile);
-                jsonDocumentMessage = JsonSerializer.Serialize(documentMessage);
-                Bot.Server.SendTextAsync(jsonDocumentMessage);
-                return;
-            }
-
-            if (!MediaFiles.TryGetValue(message.MediaGroupId, out var mediaGroupFiles))
-            {
-                mediaGroupFiles = new List<MediaFile>();
-                MediaFiles.Add(message.MediaGroupId, mediaGroupFiles);
-            }
-
-            mediaGroupFiles.Add(documentFile);
-            jsonDocumentMessage = JsonSerializer.Serialize(documentFile);
-
-            Bot.Server.SendTextAsync(jsonDocumentMessage);
-        }
-
-        private static async Task ProcessAudioMessageAsync(ITelegramBotClient botClient, Message message)
-        {
-            var audioId = message.Audio?.FileId;
-            if (string.IsNullOrEmpty(audioId)) return;
-
-            var audioInfo = await botClient.GetFileAsync(audioId);
-            var audioPath = audioInfo.FilePath;
-
-            using var audioStream = new MemoryStream();
-            await botClient.DownloadFileAsync(filePath: audioPath, destination: audioStream);
-            audioStream.Seek(0, SeekOrigin.Begin);
-
-            var audioBytes = audioStream.ToArray();
-            var audioFile = new MediaFile(Guid.NewGuid().ToString(),
-                Path.GetExtension(audioInfo.FilePath),
-                audioBytes,
-                FileType.Audio);
-
-            string? jsonAudioMessage;
-            if (string.IsNullOrEmpty(message.MediaGroupId))
-            {
-                var audioMessage = new Types.Message("MSyncBot.Telegram",
-                    1,
-                    SenderType.Telegram,
-                    Types.Enums.MessageType.Audio,
-                    new User(message.From.FirstName));
-                audioMessage.MediaFiles.Add(audioFile);
-                jsonAudioMessage = JsonSerializer.Serialize(audioMessage);
-                Bot.Server.SendTextAsync(jsonAudioMessage);
-                return;
-            }
-
-            if (!MediaFiles.TryGetValue(message.MediaGroupId, out var mediaGroupFiles))
-            {
-                mediaGroupFiles = new List<MediaFile>();
-                MediaFiles.Add(message.MediaGroupId, mediaGroupFiles);
-            }
-
-            mediaGroupFiles.Add(audioFile);
-            jsonAudioMessage = JsonSerializer.Serialize(audioFile);
-
-            Bot.Server.SendTextAsync(jsonAudioMessage);
-        }
-        
-        private static async Task ProcessVoiceMessageAsync(ITelegramBotClient botClient, Message message)
-        {
-            var voiceId = message.Voice?.FileId;
-            if (string.IsNullOrEmpty(voiceId)) return;
-
-            var voiceInfo = await botClient.GetFileAsync(voiceId);
-            var voicePath = voiceInfo.FilePath;
-
-            using var voiceStream = new MemoryStream();
-            await botClient.DownloadFileAsync(filePath: voicePath, destination: voiceStream);
-            voiceStream.Seek(0, SeekOrigin.Begin);
-
-            var voiceBytes = voiceStream.ToArray();
-            var voiceFile = new MediaFile(Guid.NewGuid().ToString(),
-                Path.GetExtension(voiceInfo.FilePath),
-                voiceBytes,
-                FileType.Voice);
-
-            var jsonVoiceMessage = JsonSerializer.Serialize(voiceFile);
-            Bot.Server.SendTextAsync(jsonVoiceMessage);
-        }
-
-        private static async Task ProcessVideoNoteMessageAsync(ITelegramBotClient botClient, Message message)
-        {
-            var videoNoteId = message.VideoNote?.FileId;
-            if (string.IsNullOrEmpty(videoNoteId)) return;
-
-            var videoNoteInfo = await botClient.GetFileAsync(videoNoteId);
-            var videoNotePath = videoNoteInfo.FilePath;
-
-            using var videoNoteStream = new MemoryStream();
-            await botClient.DownloadFileAsync(filePath: videoNotePath, destination: videoNoteStream);
-            videoNoteStream.Seek(0, SeekOrigin.Begin);
-
-            var videoNoteBytes = videoNoteStream.ToArray();
-            var videoNoteFile = new MediaFile(Guid.NewGuid().ToString(),
-                ".mp4",
-                videoNoteBytes,
-                FileType.VideoNote);
-
-            var jsonVideoNoteMessage = JsonSerializer.Serialize(videoNoteFile);
-            Bot.Server.SendTextAsync(jsonVideoNoteMessage);
-        }
-
-        public static void CountingMediaFiles(Message message)
-        {
-            if (string.IsNullOrEmpty(message.MediaGroupId))
-                return;
-
-            if (!CountMediaFiles.TryAdd(message.MediaGroupId, 1))
-                CountMediaFiles[message.MediaGroupId]++;
+            CountMediaFiles.Add(groupId, 1);
         }
     }
 }
