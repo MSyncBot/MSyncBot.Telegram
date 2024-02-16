243de0456cdbe5cab48549cbf3ff05f54ab2d8c0
mambastick <neelov.ev@mail.ru>
2024-02-17 01:09:39 +0300

    Added configuration model

---
+++ MSyncBot.Telegram/ProgramConfiguration.cs
@@ -0,0 +1,8 @@
+namespace MSyncBot.Telegram;
+
+public class ProgramConfiguration
+{
+    public string BotToken { get; set; }
+    public string ServerIp { get; set; }
+    public int ServerPort { get; set; }
+}
