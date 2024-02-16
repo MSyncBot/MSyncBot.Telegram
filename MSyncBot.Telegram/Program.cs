4ab53a65f4b36e71caa4ee1539855d79ac331be9
mambastick <neelov.ev@mail.ru>
2024-02-17 01:10:02 +0300

    Rework main method with new configuration package

--- MSyncBot.Telegram/Program.cs
+++ MSyncBot.Telegram/Program.cs
@@ -1,15 +1,50 @@
-﻿using MLoggerService;
+﻿using MConfiguration;
+using MLoggerService;
 
 namespace MSyncBot.Telegram;
 
-class Program
+internal abstract class Program
 {
-    private static async Task Main()
+    private static Task Main()
     {
         var logger = new MLogger();
+        logger.LogProcess("Initializing logger...");
+        logger.LogSuccess("Logger successfully initialized.");
 
-        var bot = new Bot.Bot("6949999276:AAEKaKjHX9kqTQDiG3j3tpDbIjKsOLJdcoY", logger);
-        await bot.StartAsync();
-        await Task.Delay(-1);
+        logger.LogProcess("Initializing program configuration...");
+        var configManager = new ConfigManager();
+        var programConfig = new ProgramConfiguration();
+        foreach (var property in typeof(ProgramConfiguration).GetProperties())
+        {
+            var propertyName = property.Name;
+            var data = configManager.Get(propertyName);
+
+            if (string.IsNullOrEmpty(data))
+            {
+                logger.LogInformation($"Enter value for {propertyName}:");
+                data = Console.ReadLine();
+            }
+            
+            property.SetValue(programConfig, Convert.ChangeType(data, property.PropertyType));
+        }
+        
+        configManager.Set(programConfig);
+
+        logger.LogSuccess("Program configuration has been initialized.");
+
+        var bot = new Bot(
+            programConfig.BotToken, 
+            programConfig.ServerIp,
+            programConfig.ServerPort,
+            logger);
+        _ = Task.Run(async () => await bot.StartAsync());
+
+        Thread.Sleep(1500); // waiting for starting bot
+
+        logger.LogInformation("Press any key to close program...");
+        Console.ReadKey();
+        bot.StopAsync().Wait();
+        
+        return Task.CompletedTask;
     }
 }
